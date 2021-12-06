9d85fdb4e042ee7d2873a23a994c003dc4da7623
Ozgur <ozgurozvaris@hotmail.com>
2021-12-06 19:32:49 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import React, {
-  useReducer, useEffect
-} from 'react';
+import React, { useReducer, useEffect } from "react";
 
-import useCombinedReducers from 'use-combined-reducers';
-import DispatchContext from "./store/dispatch";
-import ArticleList from "./conponents/ArticleList";
-import { fetchAllArticles } from "./store/articlesSelectors";
+import useCombinedReducers from "use-combined-reducers";
+import ArticleList from "./components/ArticleList";
 import { articleReducer, articleStatusReducer } from "./store/articleReducer";
 import { FETCHING_ARTICLES } from "./store/actionTypes";
+import { createContext } from "react";
+
+import axios from "axios";
+import apiURL from "../src/config/APIUrls";
 
 const App = () => {
+  const DispatchContext = createContext(null);
+
   const [state, dispatch] = useCombinedReducers({
     articles: useReducer(articleReducer, []),
-    articlesStatus: useReducer(articleStatusReducer, FETCHING_ARTICLES)
+    articlesStatus: useReducer(articleStatusReducer, FETCHING_ARTICLES),
   });
 
   const { articles, articlesStatus } = state;
 
+  const fetchAllArticles = async () => {
+    return await axios.get(apiURL.rss_abc_news_feed)
+      .then((data) => {
+        console.log("Recieved data from NEWS API.......Yay!!!!!!!");
+        return data.data.items;
+      })
+      .catch((error) => {
+        console.log("error 1->", error);
+      });
+  };
+
   useEffect(() => {
-    dispatch({ type: 'FETCHING_ARTICLES' });
+    dispatch({ type: "FETCHING_ARTICLES" });
 
     fetchAllArticles()
       .then((data) => {
-        dispatch({ type: 'ADD_ARTICLES', data });
-        dispatch({ type: 'FETCHED_ARTICLES' });
+        dispatch({ type: "ADD_ARTICLES", data });
+        dispatch({ type: "FETCHED_ARTICLES" });
       })
       .catch((error) => {
-        console.log('error ->', error);
+        console.log("error ->", error);
       });
-  }, []);   //This line is IMPORTANT since the [] makes sure it is also called only once !!!!
-
+  }, []); //This line is IMPORTANT since the [] makes sure it is also called only once !!!!
 
   return (
     <DispatchContext.Provider value={dispatch}>
-      <ArticleList articles={articles} articlesStatus={articlesStatus}/>
+      <ArticleList articles={articles} articlesStatus={articlesStatus} />
     </DispatchContext.Provider>
   );
 };
